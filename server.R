ae9d66f29b2cbfd5f6cac84c5b7a9599ed5c47b0
Magaret Oluwadare <pedogski@gmail.com>
2020-10-07 12:15:39 +0100

    VISUALIZATION OF THE BIOPSY DATA USING R
    Course project on coursera Developing Data product by JHU

---
+++ server.R
@@ -0,0 +1,24 @@
+server <- function(input, output, session) {
+  output$d3 <- renderD3({
+    biopsy %>%
+      mutate(label = !!sym(input$var)) %>%
+      group_by(label) %>%
+      tally() %>%
+      arrange(desc(n)) %>%
+      mutate(
+        y = n,
+        ylabel = prettyNum(n, big.mark = ","),
+        fill = ifelse(label != input$val, "#E69F00", "red"),
+        mouseover = "#0072B2"
+      ) %>%
+      r2d3(r2d3_file)
+  })
+  observeEvent(input$bar_clicked, {
+    updateTextInput(session, "val", value = input$bar_clicked)
+  })
+  output$table <- renderDataTable({
+    biopsy %>%
+      filter(!!sym(input$var) == input$val) %>%
+      datatable()
+  })
+}
